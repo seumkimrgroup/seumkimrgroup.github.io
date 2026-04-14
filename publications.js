b56ea4c87c3f0b684ee3d3b9b864953a31d5c2d7
srgroup <106876102+seumkimrgroup@users.noreply.github.com>
2026-04-14 18:40:06 +0900

    Create publications.js

---
+++ publications.js
@@ -0,0 +1,16 @@
+const publications = [
+  {
+    title: "Paper Title 1",
+    journal: "Nature Photonics",
+    authors: "Se-Um Kim et al.",
+    type: "international",
+    link: "https://example.com"
+  },
+  {
+    title: "Paper Title 2",
+    journal: "Optics Express",
+    authors: "Se-Um Kim et al.",
+    type: "domestic",
+    link: "https://example.com"
+  }
+];
