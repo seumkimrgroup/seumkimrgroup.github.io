4dd973004f4dfb64ad147beffb1dc7ee9c15b6fa
srgroup <106876102+seumkimrgroup@users.noreply.github.com>
2026-04-14 18:57:58 +0900

    Update publications.js

--- publications.js
+++ publications.js
@@ -7,9 +7,9 @@ const publications = [
     link: "https://example.com"
   },
   {
-    title: "Paper Title 2",
-    journal: "Optics Express",
-    authors: "Se-Um Kim et al.",
+    title: "Physical mechanism for flat-to-lenticular lens conversion in homogeneous liquid crystal cell with periodically undulated electrode",
+    journal: "Optics Express 20(2), 864-869 (2012)",
+    authors: "Jun-Hee Na, Seung Chul Park, Se-Um Kim, Yoonseuk Choi, Sin-Doo Lee",
     type: "domestic",
     link: "https://example.com"
   }
