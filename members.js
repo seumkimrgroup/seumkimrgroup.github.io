85bad404ef1b54910e60b9c2ad05e3fea432d595
srgroup <106876102+seumkimrgroup@users.noreply.github.com>
2026-04-15 15:50:24 +0900

    Update members.js

--- members.js
+++ members.js
@@ -1,1 +1,24 @@
+/* format
+{
+  name: "",
+  course: "",     // 저널명 / 특허 패밀리 / arXiv 등
+  detail: "",     // 권(호), 페이지, 번호 등
+  year: 2026,
+  authors: "",
+  type: "",       // "sci" | "non-sci" | "patent"
+  link: "",
+}
+*/
 
+const publications = [
+  {
+    title: "Low-voltage polymer-stabilized liquid crystals enabled by phase separation between liquid crystals and blends of mesogenic and non-mesogenic monomers",
+    source: "Journal of Molecular Liquids",
+    detail: "433(1), 127952",
+    year: 2025,
+    authors: "Hui Jun Jang*, Young In Lee*, Hyeon Seong Hwang, Sunjae Lee, Kanghee Won+, Se-Um Kim+",
+    type: "non-sci",
+    link: "https://www.sciencedirect.com/science/article/abs/pii/S0167732225011298"
+  },
+  {
+]
