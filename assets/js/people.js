33f9fc2453552d4bf7fafcc0de06df66e93b2806
srgroup <106876102+seumkimrgroup@users.noreply.github.com>
2026-04-20 23:38:23 +0900

    Create people.js

---
+++ assets/js/people.js
@@ -0,0 +1,520 @@
+/* format
+*/
+
+const members = [
+  {
+    name: "Se-Um Kim",
+    status: "current",
+    role: "Associate Professor",
+    email: "seumkim@seoultech.ac.kr",
+    phone: "+82-2-970-6425", office: "Mirae-Hall Unit 523",
+    joinYear: 2022, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "RESEARCH CAREER",
+        content: [
+          "2026–Present: 겸직연구원, KIST 양자기술연구단",
+          "2026–Present: Associate Professor, SeoulTech",
+          "2022–2026: Assistant Professor, SeoulTech",
+          "Staff researcher, Samsung Advanced Institute of Technology",
+          "Post-doctoral fellow, Materials Science and Engineering, University of Pennsylvania",
+          "Post-doctoral fellow, BK21 Creative Research Engineer Development for IT, Seoul National University",
+          "Post-doctoral fellow, Inter-University Semiconductor Research Center, Seoul National University",
+        ]
+      },
+      {
+        title: "EDUCATION",
+        content: [
+          "Ph.D. in Electrical and Computer Engineering, Seoul National University",
+          "B.S. in Electrical Engineering, Seoul National University"
+        ]
+      }
+    ],
+    currentAffiliation: "",
+    nextAffiliation: "",
+    slug: "s-ukim",
+  },
+  {
+    name: "I Hyeon Choi",
+    status: "current",
+    role: "Undergraduate",
+    email: "yihyeonchoi11@gmail.com",
+    phone: "", office: "",
+    joinYear: 2026, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: []
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: []
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "ihchoi",
+  },
+  {
+    name: "Ji Won Choi",
+    status: "current",
+    role: "Undergraduate",
+    email: "lxhana@seoultech.ac.kr",
+    phone: "", office: "",
+    joinYear: 2025, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: []
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "LC-based electro-optical systems",
+          "ferronematic LCs",
+          "LC synthesis"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Materials Science & Engineering",
+    nextAffiliation: "",
+    slug: "jwchoi",
+  },
+  {
+    name: "Minhye Kim",
+    status: "current",
+    role: "M.S. student",
+    email: "minhyeral14@seoultech.ac.kr",
+    phone: "", office: "",
+    joinYear: 2025, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology" 
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "LC composite materials and systems",
+          "LC elastomers",
+          "LC synthesis"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "mkim",
+  },
+  {
+    name: "Hyeon Su Kim",
+    status: "current",
+    role: "Undergraduate",
+    email: "khs1273@naver.com",
+    phone: "", office: "",
+    joinYear: 2025, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: []
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "FEM-based thermal managenemts",
+          "ML-assisted FEM optimization"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "hskim",
+  },
+  {
+    name: "Doyo Kim",
+    status: "current",
+    role: "Undergraduate",
+    email: "doyoo0413@seoultech.ac.kr",
+    phone: "", office: "",
+    joinYear: 2025, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: []
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "",
+          "LC composite materials and systems"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "dkim",
+  },
+  {
+    name: "Ae Bin Na",
+    status: "current",
+    role: "Undergraduate",
+    email: "nab5191@naver.com",
+    phone: "", office: "",
+    joinYear: 2025, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: []
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "LC composite materials and systems"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "abna",
+  },
+  {
+    name: "Hyun Jin Ko",
+    status: "current",
+    role: "Undergraduate",
+    email: "hynnk77@seoultech.ac.kr",
+    phone: "", office: "",
+    joinYear: 2025, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: []
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "LC-based electro-optical systems"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "hjko",
+  },
+  {
+    name: "Min Ji Kim",
+    status: "current",
+    role: "Undergraduate",
+    email: "kimminji0624@seoultech.ac.kr",
+    phone: "", office: "",
+    joinYear: 2024, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: []
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "FEM-based thermal managenemts",
+          "LC composite materials and systems"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "mjkim",
+  },
+  {
+    name: "Sangbaek Lee",
+    status: "current",
+    role: "Undergraduate",
+    email: "sangbaek62@gmail.com",
+    phone: "", office: "",
+    joinYear: 2024, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: []
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "FEM based thermal managenemts",
+          "PINN-assisted optimization of FEM"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "slee",
+  },
+  {
+    name: "Sunjae Lee",
+    status: "former",
+    role: "Undergraduate",
+    email: "ddwd1473@naver.com",
+    phone: "", office: "",
+    joinYear: 2024, leaveYear: 2025,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology"
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "LC composite materials and systems",
+          "Polymer stabilized liquid crystals"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "Wuxi Hengxin",
+    slug: "slee2",
+  },
+  {
+    name: "Ji Hwan Kim",
+    status: "current",
+    role: "Undergraduate",
+    email: "dyne0822@gmail.com",
+    phone: "", office: "",
+    joinYear: 2024, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: []
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "LC composite materials and systems",
+          "Polymer stabilized liquid crystals"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "jhkim",
+  },
+  {
+    name: "Hyo Seok Oh",
+    status: "current",
+    role: "M.S. student",
+    email: "ohs07@naver.com",
+    phone: "", office: "",
+    joinYear: 2024, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology"
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "Electrochromic devices",
+          "Reflective displays"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "hsoh",
+  },
+  {
+    name: "Hui Jun Jang",
+    status: "former",
+    role: "Undergraduate",
+    email: "",
+    phone: "", office: "",
+    joinYear: 2024, leaveYear: 2025,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology"
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "LC composite materials and systems",
+          "Polymer stabilized liquid crystals"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "LG Energy Solution",
+    slug: "hjjang",
+  },
+  {
+    name: "Yong Jin Hwang",
+    status: "former",
+    role: "Undergraduate",
+    email: "",
+    phone: "", office: "",
+    joinYear: 2023, leaveYear: 2025,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology"
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "FEM based thermal managenemts"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "Autonics",
+    slug: "yjhwang",
+  },
+  {
+    name: "Hyeon Seong Hwang",
+    status: "current",
+    role: "M.S. student",
+    email: "he9312@seoultech.ac.kr",
+    phone: "", office: "",
+    joinYear: 2023, leaveYear: null,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology"
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "LC composite materials and systems",
+          "CLC-based optical filters",
+          "Polymer stabilized liquid crystals"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "",
+    slug: "hshwang",
+  },
+  {
+    name: "Hyun Mok Cho",
+    status: "former",
+    role: "Undergraduate",
+    email: "",
+    phone: "", office: "",
+    joinYear: 2023, leaveYear: 2025,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology"
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: [
+          "electrochromic devices"
+        ]
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "MTS",
+    slug: "hmcho",
+  },
+  {
+    name: "Jun Ho Lee",
+    status: "former",
+    role: "Undergraduate",
+    email: "",
+    phone: "", office: "",
+    joinYear: 2023, leaveYear: 2024,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology"
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: []
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "Hyundai Motors",
+    slug: "jhlee",
+  },
+  {
+    name: "Jung Eun Lee",
+    status: "former",
+    role: "Undergraduate",
+    email: "",
+    phone: "", office: "",
+    joinYear: 2023, leaveYear: 2024,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology"
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: []
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "LIG Nex1",
+    slug: "jelee",
+  },
+  {
+    name: "Min-Seok Park",
+    status: "former",
+    role: "Undergraduate",
+    email: "",
+    phone: "", office: "",
+    joinYear: 2022, leaveYear: 2023,
+    image: "",
+    description: [
+      {
+        title: "EDUCATION",
+        content: [
+          "B.S. in Electrical and Informationg Engineering, Seoul National University of Science and Technology"
+        ]
+      },
+      {
+        title: "RESEARCH INTERESTS",
+        content: []
+      }
+    ],
+    currentAffiliation: "Department of Electrical and Information Engineering",
+    nextAffiliation: "Hitachi High-Tech in Korea",
+    slug: "m-spark",
+  }
+];
