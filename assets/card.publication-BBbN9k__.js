accc04796f206b6352ab2a97c9c0fecf5caf9a11
seumkimrgroup <seumkimrgroup@users.noreply.github.com>
2026-05-09 11:00:22 +0000

    deploy: seumkimrgroup/seumkimrgroup-build@972f6be96fcb0e1edd5434c7e1b294905d691a77

--- assets/card.publication-BBbN9k__.js
+++ assets/card.publication-BBbN9k__.js
@@ -1,4 +1,4 @@
-import{e as a}from"./data-Dfp7C0L4.js";function i(r){const n=`
+import{e as a}from"./data-C8g29wkK.js";function i(r){const n=`
     <h5>${a(r.title||"")}</h5>
     <p class="text-secondary">${a(r.source||"")}</p>
     <p>${a(r.authors||"")}</p>
