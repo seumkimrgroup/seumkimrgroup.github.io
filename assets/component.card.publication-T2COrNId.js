5a6e9686bb793c0d294fd288ef7310b9fead62d1
seumkimrgroup <seumkimrgroup@users.noreply.github.com>
2026-05-11 14:26:01 +0000

    deploy: seumkimrgroup/seumkimrgroup-build@735627bdb1f0646e62d4868d483b6e7ec04bc190

--- assets/component.card.publication-T2COrNId.js
+++ assets/component.card.publication-T2COrNId.js
@@ -1,4 +1,4 @@
-import{e as a}from"./util.data-D40mZLs_.js";function i(r){const n=`
+import{e as a}from"./util.data-gk1xnSV2.js";function i(r){const n=`
     <h5>${a(r.title||"")}</h5>
     <p class="text-secondary">${a(r.source||"")}</p>
     <p>${a(r.authors||"")}</p>
