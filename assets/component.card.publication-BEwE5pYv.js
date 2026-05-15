c140f9a7597eedec21263c0a894194956292b1f7
seumkimrgroup <seumkimrgroup@users.noreply.github.com>
2026-05-15 07:45:02 +0000

    deploy: seumkimrgroup/seumkimrgroup-build@36650838618a8eaa37df3b79fccd117637812890

--- assets/component.card.publication-BEwE5pYv.js
+++ assets/component.card.publication-BEwE5pYv.js
@@ -1,4 +1,4 @@
-import{e as a}from"./util.data-QMv_boDC.js";function i(r){const n=`
+import{e as a}from"./util.data-CASC_6qb.js";function i(r){const n=`
     <h5>${a(r.title||"")}</h5>
     <p class="text-secondary">${a(r.source||"")}</p>
     <p>${a(r.authors||"")}</p>
