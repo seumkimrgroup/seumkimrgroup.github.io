75538bde306ed31560b400bb92710fdf7188629b
seumkimrgroup <seumkimrgroup@users.noreply.github.com>
2026-05-15 11:42:32 +0000

    deploy: seumkimrgroup/seumkimrgroup-build@bd2c2f9c3e0d2c462ff8f6ca8523458fd206fb2e

--- assets/component.card.publication-FsR9e0fM.js
+++ assets/component.card.publication-FsR9e0fM.js
@@ -1,4 +1,4 @@
-import{e as a}from"./util.data-B6H06nSc.js";function i(r){const n=`
+import{e as a}from"./util.data-BcTPkeiS.js";function i(r){const n=`
     <h5>${a(r.title||"")}</h5>
     <p class="text-secondary">${a(r.source||"")}</p>
     <p>${a(r.authors||"")}</p>
