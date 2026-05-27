597534c7fcba09baa5387d2bc2e3f3d16ecfd6e2
seumkimrgroup <seumkimrgroup@users.noreply.github.com>
2026-05-27 14:31:42 +0000

    deploy: seumkimrgroup/seumkimrgroup-build@4f8d2719de62d9f1d6236acd7602afcf6efe4f22

--- assets/people-B0WoGYr6.js
+++ assets/people-B0WoGYr6.js
@@ -1,4 +1,4 @@
-import{c as e,i as t,l as n,r,s as i,t as a}from"./jsx-runtime-CdNpcH6_.js";import{i as o,n as s,o as c,r as l}from"./component.iconlinks-B5_8JaUh.js";var u=t(),d=a();n(),e(),i();var f=document.getElementById(`members`),p=document.getElementById(`alumni`);function m(){let e=document.querySelector(`#pi`);e&&(e.innerHTML=`
+import{c as e,i as t,l as n,r,s as i,t as a}from"./jsx-runtime-CdkZBqYi.js";import{i as o,n as s,o as c,r as l}from"./component.iconlinks-DPwJN1y-.js";var u=t(),d=a();n(),e(),i();var f=document.getElementById(`members`),p=document.getElementById(`alumni`);function m(){let e=document.querySelector(`#pi`);e&&(e.innerHTML=`
     <h6>Principal Investigator</h6>
     <div class="split">
       <div class="media">
